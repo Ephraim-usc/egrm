9e6daf96f57324aa0c2564867810d89e757af417
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2019-12-16 21:10:39 -0800

    Update read_and_write.py

--- read_and_write.py
+++ read_and_write.py
@@ -2,4 +2,10 @@
 
 
 ### PLINK
-def write_simulation_PLINK(simulation, file):
+def write_simulation_PLINK(genotype_matrix, file):
+  for i in range(M_obs):
+  string = "1 SNP" + str(i+1) + " " + str(math.ceil(variants_obs[i])) + " A" + " T "
+  string = string + " ".join(map(str, genotype_matrix_obs[i])) + "\n"
+  tmp_file = open(out_file + ".relate/" + out_file + ".haps",'a')
+  bytes = tmp_file.write(string)
+  tmp_file.close()
