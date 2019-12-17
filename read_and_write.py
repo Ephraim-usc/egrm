fb8aaf48f01291be6251498f937f6eab0d4f49e0
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2019-12-17 13:51:07 -0800

    Update read_and_write.py

--- read_and_write.py
+++ read_and_write.py
@@ -1,15 +1,56 @@
 ### importing packages
 import numpy as np
+import os
 
-### PLINK
-def write_simulation_PLINK(simulation, idx, file):
-  X = np.transpose(simulation["hapdata"]["genotype_matrix"][idx])
-  y = simulation["phenotypes"]["y"]
+### PLINK and GCTA
+def write_plink(simulation, idx, file):
+  idx_diploid = idx * 2; idx_diploid = np.concatenate([idx_diploid, idx_diploid + 1])
+  idx_diploid = np.sort(idx_diploid)
+  X = np.transpose(simulation["hapdata"]["genotype_matrix_diploid"][idx_diploid])
+  y = simulation["phenotypes"]["y_diploid"]
+  variants = simulation["hapdata"]["variants"]
   N = X.shape[0]
   
   ped_file = open(file + ".ped", 'a')
   for i in range(N):
-    string = "msprime " + str(i+1) + " 0 0 0 " + str(y[i])
+    string = "msprime " + str(i+1) + " 0 0 0 " + str(y[i]) + " "
     string = string + " ".join(map(str, X[i])) + "\n"
     bytes = ped_file.write(string)
   ped_file.close()
+  
+  fam_file = open(file + ".fam", 'a')
+  for i in range(N):
+    string = "msprime " + str(i+1) + " 0 0 0 " + str(y[i]) + "\n"
+    bytes = fam_file.write(string)
+  fam_file.close()
+  
+  phen_file = open(file + ".phen", 'a')
+  for i in range(N):
+    string = "msprime " + str(i+1) + " " + str(y[i]) + "\n"
+    bytes = phen_file.write(string)
+  phen_file.close()
+  
+  map_file = open(file + ".map", 'a')
+  for i in idx:
+    string = "1 snp" + str(i+1) + " 0 " + str(variants[i]) + "\n"
+    bytes = map_file.write(string)
+  map_file.close()
+  
+  bim_file = open(file + ".bim", 'a')
+  for i in idx:
+    string = "1 snp" + str(i+1) + " 0 " + str(variants[i]) + " A T\n"
+    bytes = bim_file.write(string)
+  bim_file.close()
+
+
+def gcta64(file):
+  os.system("/home/rcf-40/caoqifan/cc2/plink-1.07-x86_64/plink --file " + 
+            file + " --make-bed --out " + file + " --noweb")
+  
+  os.system("/home/rcf-40/caoqifan/cc2/gcta_1.93.0beta/gcta64 --bfile " + 
+            file + " --make-grm-bin --out " + file)
+  
+  os.system("/home/rcf-40/caoqifan/cc2/gcta_1.93.0beta/gcta64  --reml  --grm " + 
+            file + " --out " + file + " --pheno " + file + ".phen")
+  
+  
