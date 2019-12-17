5c59ceeef3967a50d2225ee4484d59ec27d73b8a
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2019-12-16 20:47:01 -0800

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -58,7 +58,69 @@ def simulate_hapdata(l = l, N = N, mutation_rate = mutation_rate, recomb_rate = 
             "MAFs":MAFs, "MACs":MACs, "variants":variants, "M":M}
 
 def simulate_phenotypes(hapdata, h2g = h2g, cas_ratio = cas_ratio, Alpha = Alpha):
+    genotype_matrix = hapdata["genotype_matrix"]
+    M = hapdata["M"]
+    MAFs = hapdata["MAFs"]
     
+    M_cas = int(round(M * cas_ratio))
+    weights = np.multiply(np.power(MAFs, Alpha), np.power(1-MAFs, Alpha))
+    weights /= weights.sum()
+    cass = np.random.choice(range(M), M_cas, replace = False, p = weights); cass.sort()
+    
+    X_cas = np.transpose(genotype_matrix[cass])
+    Z_cas = X_cas - np.mean(X_cas, axis=0)
+    Z_cas = Z_cas / np.std(Z_cas, axis=0)
+    
+    betas = np.random.normal(scale = np.sqrt(h2g / M_cas), size = (M_cas, 1))
+    
+    G = np.dot(X_cas, betas); G.shape = (N, )
+    s2g = np.var(G, ddof = 1)
+    s2e = s2g * (1/h2g - 1)
+    
+    e = np.random.normal(scale=np.sqrt(s2e), size=N)
+    y = G + e
+    
+    return {"M_cas":M_cas, "cass":cass, "Z_cas":Z_cas, "betas":betas, "y":y}
+
+def simulate_observations(hapdata, obs_ratio = obs_ratio, Beta = Beta):
+    genotype_matrix = hapdata["genotype_matrix"]
+    variants = hapdata["variants"]
+    MAFs = hapdata["MAFs"]
+    MACs = hapdata["MACs"]
+    
+    idx_5 = MACs >= 0.005
+    M_5 = idx_5.sum()
+    MAFs_5 = MAFs[idx_5]
+    
+    variants_ceil = np.ceil(variants)
+    overlapped = np.insert(variants_ceil[:-1] == variants_ceil[1:], 1, False)
+    non_overlapped = np.logical_not(overlapped)
+    
+    observable =  np.logical_and(idx_5, non_overlapped)
+    M_observable = observable.sum()
+    
+    weights = np.multiply(np.power(MAFs, Beta), np.power(1-MAFs, Beta))
+    weights = np.multiply(weights, observable)
+    weights /= weights.sum()
+    
+    M_obs = int(round(M_observable * obs_ratio))
+    obss = np.random.choice(np.where(observable)[0], M_obs, replace = False, p = weights[observable]); obss.sort()
+    
+    X_obs = np.transpose(genotype_matrix[obss])
+    Z_obs = X_obs - np.mean(X_obs, axis=0)
+    Z_obs = Z_obs / np.std(Z_obs, axis=0)
+    
+    return {"M_5":M_5, "M_obs":M_obs, "obss":obss, "Z_obs":Z_obs}
+
+def simulate(l = l, N = N, mutation_rate = mutation_rate, recomb_rate = recomb_rate,
+            h2g = h2g, cas_ratio = cas_ratio, Alpha = Alpha,
+            obs_ratio = obs_ratio, Beta = Beta):
+    hapdata = simulate_hapdata()
+    phenotypes = simulate_phenotypes(hapdata)
+    observations = simulate_observations(hapdata)
+    
+    return {"hapdata":hapdata, "phenotypes":phenotypes, "observations":observations}
+
 
 
 
