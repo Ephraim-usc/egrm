d40d68bccdf3170e9799f9c159fcda7c6d4cf08a
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-01-28 19:54:58 -0800

    Update blup.py

--- egrm/blup.py
+++ egrm/blup.py
@@ -14,6 +14,14 @@ def BLUP(K, y_train, trains, tests, h2 = 0.9):
   y_ = np.dot(Kt, np.dot(Vi, y_train))
   return y_
 
+def h_estimate(K, y, N):
+  y_norm = y - y.mean(); y_norm = y_norm / y_norm.std()
+  K2 = np.diag(np.dot(K, K)).sum()
+  yTKy = np.dot(np.dot(np.transpose(y_norm), K), y_norm)
+  yTy = np.dot(np.transpose(y_norm), y_norm)
+  buffer = (yTKy - yTy) / (K2 - N)
+  return buffer
+
 def test(simulation, repeats = 1000):
   N = simulation["parameters"]["N"]
   K_cas = simulation["Ks"]["K_cas"]
@@ -33,6 +41,12 @@ def test(simulation, repeats = 1000):
   corr = table.corr(method ='pearson')
   
   y = simulation["phenotypes"]["y"]
+  h_estimation = {'K_cas':h_estimate(K_cas, y, N), 
+                  'K_obs':h_estimate(K_obs, y, N),
+                  'Km':h_estimate(Km, y, N),
+                  'Km_relate':h_estimate(Km_relate, y, N),
+                  'Km_tsinfer':h_estimate(Km_tsinfer, y, N)} 
+  
   a = []
   b = []
   c = []
@@ -62,14 +76,23 @@ def test(simulation, repeats = 1000):
   d = np.array(d)
   e = np.array(e)
   blup = {"K_cas":a, "K_obs":b, "Km":c, "Km_relate":d, "Km_tsinfer":e}
-  simulation["tests"] = {"corr":corr, "blup":blup}
+  simulation["tests"] = {"corr":corr, "blup":blup, 'h_estimation':h_estimation}
 
 def summary(simulation):
   summ = "==========\nparameters \n==========\n"
   summ += "\n".join([str(x) + "\t" + str(simulation["parameters"][x]) for x in simulation["parameters"].keys()]) + "\n"
+  
   summ += "==========\nK matrix correlations \n==========\n"
   summ += str(simulation["tests"]["corr"]) + "\n"
   tmp = simulation["tests"]["blup"]["K_cas"]
+  
+  summ += "==========\nheritability estimation \n==========\n"
+  summ += "K_cas\t" + str(round(simulation["tests"]["h_estimation"]["K_cas"], 4)) + "\n"
+  summ += "K_obs\t" + str(round(simulation["tests"]["h_estimation"]["K_obs"], 4)) + "\n"
+  summ += "Km\t" + str(round(simulation["tests"]["h_estimation"]["Km"], 4)) + "\n"
+  summ += "Km_relate\t" + str(round(simulation["tests"]["h_estimation"]["Km_relate"], 4)) + "\n"
+  summ += "Km_tsinfer\t" + str(round(simulation["tests"]["h_estimation"]["Km_tsinfer"], 4)) + "\n"
+  
   summ += "==========\nBLUP accuracy \n==========\n"
   summ += "K_cas\t" + str(round(tmp.mean(), 4)) + " +- " + str(round(tmp.std(), 4)) + "\n"
   tmp = simulation["tests"]["blup"]["K_obs"]
