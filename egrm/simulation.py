6b99a6371de1bd54157bffa48c0fde1d9c9d9a4a
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-05-18 00:01:51 -0700

    Update simulation.py

--- egrm/simulation.py
+++ egrm/simulation.py
@@ -31,7 +31,7 @@ T_B = 140e3 / generation_time
 T_AF = 220e3 / generation_time
 N_EU = N_EU0 / math.exp(-r_EU * T_EU_AS)
 
-### parameters
+### simulations
 def simulate_hapdata(l = l, N = N, mutation_rate = mutation_rate, recomb_rate = recomb_rate):
     population_configurations = [msprime.PopulationConfiguration(sample_size = N, initial_size = N_EU, growth_rate = r_EU)]
     
@@ -39,57 +39,38 @@ def simulate_hapdata(l = l, N = N, mutation_rate = mutation_rate, recomb_rate = 
                    msprime.PopulationParametersChange(time=T_B, initial_size = N_AF, growth_rate=0),
                    msprime.PopulationParametersChange(time=T_AF, initial_size = N_A, growth_rate=0)]
     
-    tree_sequence = msprime.simulate(length = l, population_configurations = population_configurations, 
+    trees = msprime.simulate(length = l, population_configurations = population_configurations, 
                                      recombination_rate = recomb_rate, mutation_rate = mutation_rate, 
                                      demographic_events = demo_events)
+    variants = trees.variants()
     
-    genotype_matrix = tree_sequence.genotype_matrix().astype("uint16")
-    MAFs = genotype_matrix.mean(axis = 1); MACs = np.array(list(map(min, MAFs, 1-MAFs)))
-    variants = np.array([v.position for v in tree_sequence.variants()])
-    M = len(variants)
+    MAFs = np.array([v.genotypes.mean() for v in trees.variants()])
+    loci = np.array([v.position for v in trees.variants()])
+    M = len(loci)
     
-    return {"tree_sequence":tree_sequence, "genotype_matrix":genotype_matrix, 
-            "MAFs":MAFs, "MACs":MACs, "variants":variants, "M":M}
+    return {"trees":trees, "MAFs":MAFs, "loci":loci, "M":M}
 
-def simulate_phenotypes(hapdata, h2g = h2g, cas_ratio = cas_ratio, Alpha = Alpha):
-    genotype_matrix = hapdata["genotype_matrix"]
-    N = genotype_matrix.shape[1]
-    M = hapdata["M"]
-    MAFs = hapdata["MAFs"]
-    
-    M_cas = int(round(M * cas_ratio))
-    weights = np.multiply(np.power(MAFs, Alpha), np.power(1-MAFs, Alpha))
-    weights /= weights.sum()
-    cass = np.random.choice(range(M), M_cas, replace = False, p = weights); cass.sort()
-    
-    X_cas = np.transpose(genotype_matrix[cass])
-    Z_cas = np.transpose(genotype_matrix[cass]).astype("float")
-    Z_cas -= Z_cas.mean(axis=0)
-    Z_cas /= Z_cas.std(axis=0)
-    
-    betas = np.random.normal(scale = np.sqrt(h2g / M_cas), size = (M_cas, 1))
-    
-    G = np.dot(X_cas, betas); G.shape = (N, )
-    s2g = np.var(G, ddof = 1)
-    s2e = s2g * (1/h2g - 1)
-    
-    e = np.random.normal(scale=np.sqrt(s2e), size=N)
-    y = G + e
-    
-    return {"M_cas":M_cas, "cass":cass, "Z_cas":Z_cas, "betas":betas, "y":y}
+def getX(hapdata, idx):
+  N = hapdata["trees"].num_samples
+  X = np.zeros((N, len(idx)))
+  variants = hapdata["trees"].variants()
+  index = 0; num = 0
+  for v in variants:
+    if index in idx:
+      X[:, num] = v.genotypes; num += 1
+    index += 1
+  return X
 
 def simulate_observations(hapdata, obs_ratio = obs_ratio, Beta = Beta):
-    genotype_matrix = hapdata["genotype_matrix"]
-    variants = hapdata["variants"]
+    loci = hapdata["loci"]
     MAFs = hapdata["MAFs"]
-    MACs = hapdata["MACs"]
     
-    idx_5 = MACs >= 0.005
+    idx_5 = MAFs >= 0.005
     M_5 = idx_5.sum()
     MAFs_5 = MAFs[idx_5]
     
-    variants_ceil = np.ceil(variants)
-    overlapped = np.insert(variants_ceil[:-1] == variants_ceil[1:], 1, False)
+    loci_ceil = np.ceil(loci)
+    overlapped = np.insert(loci_ceil[:-1] == loci_ceil[1:], 1, False)
     non_overlapped = np.logical_not(overlapped)
     
     observable =  np.logical_and(idx_5, non_overlapped)
@@ -102,11 +83,29 @@ def simulate_observations(hapdata, obs_ratio = obs_ratio, Beta = Beta):
     M_obs = int(round(M_observable * obs_ratio))
     obss = np.random.choice(np.where(observable)[0], M_obs, replace = False, p = weights[observable]); obss.sort()
     
-    Z_obs = np.transpose(genotype_matrix[obss]).astype("float")
-    Z_obs -= Z_obs.mean(axis=0)
-    Z_obs /= Z_obs.std(axis=0)
+    return {"M_5":M_5, "M_obs":M_obs, "obss":obss}
+
+def simulate_phenotypes(hapdata, h2g = h2g, cas_ratio = cas_ratio, Alpha = Alpha):
+    N = hapdata["trees"].num_samples
+    M = hapdata["M"]
+    MAFs = hapdata["MAFs"]
     
-    return {"M_5":M_5, "M_obs":M_obs, "obss":obss, "Z_obs":Z_obs}
+    M_cas = int(round(M * cas_ratio))
+    weights = np.multiply(np.power(MAFs, Alpha), np.power(1-MAFs, Alpha))
+    weights /= weights.sum()
+    cass = np.random.choice(range(M), M_cas, replace = False, p = weights); cass.sort()
+    X_cas = getX(hapdata, cass)
+    
+    betas = np.random.normal(scale = np.sqrt(h2g / M_cas), size = (M_cas, 1))
+    
+    G = np.dot(X_cas, betas); G.shape = (N, )
+    s2g = np.var(G, ddof = 1)
+    s2e = s2g * (1/h2g - 1)
+    
+    e = np.random.normal(scale=np.sqrt(s2e), size=N)
+    y = G + e
+    
+    return {"M_cas":M_cas, "cass":cass, "betas":betas, "y":y}
 
 def simulate(l = l, N = N, mutation_rate = mutation_rate, recomb_rate = recomb_rate,
             h2g = h2g, cas_ratio = cas_ratio, Alpha = Alpha,
@@ -117,11 +116,18 @@ def simulate(l = l, N = N, mutation_rate = mutation_rate, recomb_rate = recomb_r
     observations = simulate_observations(hapdata, obs_ratio = obs_ratio, Beta = Beta)
     simulation = {"parameters":parameters, "hapdata":hapdata, "phenotypes":phenotypes, "observations":observations}
     
-    Z_cas = phenotypes["Z_cas"]
-    Z_obs = observations["Z_obs"]
-    
+    Z_cas = getX(hapdata, phenotypes["cass"])
+    Z_cas -= Z_cas.mean()
+    Z_cas /= Z_cas.std()
     K_cas = np.dot(Z_cas, np.transpose(Z_cas)) / Z_cas.shape[1]
+    del Z_cas
+    
+    Z_obs = getX(hapdata, observations["obss"])
+    Z_obs -= Z_obs.mean()
+    Z_obs -= Z_obs.mean()
     K_obs = np.dot(Z_obs, np.transpose(Z_obs)) / Z_obs.shape[1]
+    del Z_obs
+    
     Ks = {"K_cas":K_cas, "K_cas_M":simulation["phenotypes"]["M_cas"], 
           "K_obs":K_obs, "K_obs_M":simulation["observations"]["M_obs"]}
     
@@ -138,21 +144,7 @@ def make_diploid(simulation):
     
     simulation['diploid'] = {'maternals':maternals, 'paternals':paternals, 'y_diploid':y_diploid}
 
-    '''
-    maternals_snp = np.arange(0, M * 2 - 1, 2)
-    paternals_snp = np.arange(1, M * 2, 2)
-    maternals_hap = np.arange(0, N - 1, 2)
-    paternals_hap = np.arange(1, N, 2)
-
-    genotype_matrix_diploid = np.empty([M * 2, N // 2]).astype("uint16")
-    genotype_matrix_diploid[maternals_snp, :] = genotype_matrix[:, maternals_hap]
-    genotype_matrix_diploid[paternals_snp, :] = genotype_matrix[:, paternals_hap]
-    simulation["hapdata"]["genotype_matrix_diploid"] = genotype_matrix_diploid
-
-    y_diploid = y[maternals_hap] + y[paternals_hap]
-    simulation["phenotypes"]["y_diploid"] = y_diploid
-    '''
-
+'''
 def merge(simulations):
     ls = list(x["parameters"]["l"] for x in simulations)
     simulation = {}
@@ -169,4 +161,4 @@ def merge(simulations):
     simulation["Ks"]["Km_relate"] = sum([ls[i] * simulations[i]["Ks"]["Km_relate"] for i in range(len(ls))]) / sum(ls)
     
     return(simulation)
-
+'''
