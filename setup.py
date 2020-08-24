3e018d4f1533b59c133d31d46d5201bdab2c5f33
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-08-24 16:08:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setup(name='egrm',
       install_requires=[
           'tskit', 'tqdm', 'msprime'
       ],
-      scripts=['bin/trees2egrm', 'bin/workflow', 'bin/summarize'],
+      scripts=['bin/trees2egrm', 'bin/trees2tmrca', 'bin/workflow', 'bin/summarize'],
       ext_modules=[Extension('matrix', ['src/matrix.c'])],
       zip_safe=False)
