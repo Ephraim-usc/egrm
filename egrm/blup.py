e1bda77f8d1cb4b5ebebfbecf4be9fbccc3e538a
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-10-01 12:06:32 -0500

    Update blup.py

--- egrm/blup.py
+++ egrm/blup.py
@@ -27,7 +27,7 @@ def getKs(simulation):
 def BLUP(K, y_train, trains, tests, h2 = 0.9):
   N_train = len(trains)
   
-  I = (1/h2 - h2) * np.identity(N_train)
+  I = (1/h2 - 1) * np.identity(N_train)
   V = K[trains, :][:, trains] + I
   
   Kt = K[tests, :][:, trains]
