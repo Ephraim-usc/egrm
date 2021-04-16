14e8bfff16160ff4c5e6f8749a05efc4630a5bd7
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2021-04-16 18:09:15 -0500

    Update egrm.py

--- egrm/egrm.py
+++ egrm/egrm.py
@@ -126,82 +126,98 @@ def mTMRCA_C(trees, log = None,
   return mtmrca, total_l
 
 
-### without C extension (to be added)
-varGRM = varGRM_C
-
-def TMRCA(tree):
-  N = tree.num_samples()
-  rel_nodes = list(tree.nodes())
-  times = [tree.time(node) for node in rel_nodes]
-  rel_nodes = np.array(rel_nodes)[np.flip(np.argsort(times))]
-  tmrca = np.zeros([N, N])
-  for c in rel_nodes:
-    descendants = list(tree.samples(c))
-    n = len(descendants)
-    if(n == 0):
-      continue
-    q = tree.time(c)
-    tmrca[np.ix_(descendants, descendants)] = q
-  return tmrca
-
-def mTMRCA(trees, file = None):
+### without C extension
+def varGRM(trees, log = None, 
+             rlim = 0, alim = math.inf, 
+             left = 0, right = math.inf, 
+             map_func = (lambda x:x), g = (lambda x: 1/(x*(1-x))), 
+             var = True, sft = False):
+  if map_func == None or map_func.mapped == False:
+    map_func = (lambda x:x)
+  
   N = trees.num_samples
-  buffer = np.zeros([N, N])
+  egrm = np.zeros([N, N])
+  egrm2 = np.zeros([N, N])
+  tmp1 = np.zeros(N)
+  tmp2 = 0
+  total_mu = 0
+  pbar = tqdm.tqdm(total = trees.num_trees, 
+                   bar_format = '{l_bar}{bar:30}{r_bar}{bar:-30b}',
+                   miniters = trees.num_trees // 100,
+                   file = log)
+  
+  trees_ = trees.trees()
+  if sft: next(trees_)
+  
+  for tree in trees_:
+    if tree.total_branch_length == 0: continue
+    l = - map_func(max(left, tree.interval[0])) + map_func(min(right, tree.interval[1]))
+    if l == 0: continue
+    
+    for c in tree.nodes():
+      descendants = list(tree.samples(c))
+      n = len(descendants)
+      if(n == 0 or n == N): continue
+      t = max(0, min(alim, tree.time(tree.parent(c))) - max(rlim, tree.time(c)))
+      mu = l * t * 1e-8
+      p = float(n/N)
+      egrm[np._ix(descendants, descendants)] += mu * g(p)
+      if var:
+        egrm2[np._ix(descendants, descendants)] += mu * np.power(g(p), 2) * np.power((1 - 2*p), 2)
+        tmp1[descendants] += mu * np.power(g(p), 2) * (np.power(p, 2) - 2 * np.power(p, 3))
+        tmp2 += mu * np.power(g(p), 2) * np.power(p, 4)
+      total_mu += mu
+    pbar.update(1)
+  
+  egrm /= total_mu
+  egrm2 /= total_mu
+  tmp1 /= total_mu
+  tmp2 /= total_mu
+  
+  e = np.reciprocal(np.random.poisson(lam=total_mu, size=10000).astype("float")).mean()
+  egrm_final = center(egrm)
+  vargrm_final = e * (egrm2 + np.tile(tmp1, (N, 1)) + np.tile(tmp1, (N, 1)).transpose() + tmp2 - np.power(egrm_final, 2))
+  
+  pbar.close()
+  return egrm_final, vargrm_final, total_mu
+
+
+def mTMRCA_C(trees, log = None, 
+             left = 0, right = math.inf, 
+             map_func = (lambda x:x), sft = False):
+  if map_func == None or map_func.mapped == False:
+    map_func = (lambda x:x)
+  
+  N = trees.num_samples
+  mtmrca = np.zeros([N, N])
+  tmp = 0
   total_l = 0
   pbar = tqdm.tqdm(total = trees.num_trees, 
                    bar_format = '{l_bar}{bar:30}{r_bar}{bar:-30b}',
                    miniters = trees.num_trees // 100,
-                   file = file)
-  for tree in trees.trees():
-    if tree.total_branch_length == 0:
-      continue
-    l = (tree.interval[1] - tree.interval[0])
+                   file = log)
+  
+  trees_ = trees.trees()
+  if sft: next(trees_)
+  
+  for tree in trees_:
+    if tree.total_branch_length == 0: continue
+    l = - map_func(max(left, tree.interval[0])) + map_func(min(right, tree.interval[1]))
+    if l == 0: continue
+    
+    height = 0
+    for c in tree.nodes():
+      descendants = list(tree.samples(c))
+      n = len(descendants)
+      if(n == 0 or n == N): continue
+      t = tree.time(tree.parent(c)) - tree.time(c)
+      height = max(height, tree.time(tree.parent(c)))
+      mtmrca[np._ix(descendants, descendants)] += t * l
+    tmp += height * l
     total_l += l
-    K = TMRCA(tree)
-    buffer += K * l
     pbar.update(1)
-  buffer /= total_l
+  
+  mtmrca = tmp - mtmrca
+  mtmrca /= total_l
   pbar.close()
-  return buffer, total_l
-
-def gmTMRCA(trees, file = None):
-  N = trees.num_samples
-  buffer = np.zeros([N, N])
-  total_l = 0
-  pbar = tqdm.tqdm(total = trees.num_trees, 
-                   bar_format = '{l_bar}{bar:30}{r_bar}{bar:-30b}',
-                   miniters = trees.num_trees // 100,
-                   file = file)
-  for tree in trees.trees():
-    if tree.total_branch_length == 0:
-      continue
-    l = (tree.interval[1] - tree.interval[0])
-    total_l += l
-    K = TMRCA(tree); np.fill_diagonal(K, 1)
-    buffer += np.log(K) * l
-    pbar.update(1)
-  buffer /= total_l
-  buffer = np.exp(buffer); np.fill_diagonal(buffer, 0)
-  pbar.close()
-  return buffer, total_l
-
-def hmTMRCA(trees, file = None):
-  N = trees.num_samples
-  buffer = np.zeros([N, N])
-  total_l = 0
-  pbar = tqdm.tqdm(total = trees.num_trees, 
-                   bar_format = '{l_bar}{bar:30}{r_bar}{bar:-30b}',
-                   miniters = trees.num_trees // 100,
-                   file = file)
-  for tree in trees.trees():
-    if tree.total_branch_length == 0:
-      continue
-    l = (tree.interval[1] - tree.interval[0])
-    total_l += l
-    K = TMRCA(tree); np.fill_diagonal(K, 1)
-    buffer += np.power(K, -1) * l
-    pbar.update(1)
-  buffer /= total_l
-  buffer = np.power(buffer, -1); np.fill_diagonal(buffer, 0)
-  pbar.close()
-  return buffer, total_l
+  return mtmrca, total_l
