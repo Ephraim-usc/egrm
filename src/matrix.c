c8441be34cd8f6db374803f323ed341703e3d19c
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2021-05-07 03:55:28 -0500

    Update matrix.c

--- src/matrix.c
+++ src/matrix.c
@@ -235,7 +235,7 @@ static PyObject* py_export_ndarray(PyObject* self, PyObject* args)
   import_array();
   ITYPE dims[2];
   dims[0] = dims[1] = n;
-  PyObject *py_ndarray = PyArray_SimpleNewFromData(n*n, dims, NPY_INT64, data)
+  PyObject *py_ndarray = PyArray_SimpleNewFromData(n*n, dims, NPY_INT64, data);
   
   return py_ndarray;
 }
